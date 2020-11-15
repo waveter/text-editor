e934b25443e7f2793de07effcb134d027f58086a
waveter <mrnguyenbathuy@outlook.com>
2020-11-15 23:07:21 +0700

    Change to use Quill

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,213 +1,137 @@
 import React from "react";
 import "./TextEditor.css";
-// Require Editor JS files.
-import "froala-editor/js/froala_editor.pkgd.min.js";
-import "froala-editor/js/plugins.pkgd.min.js";
-import "froala-editor/js/third_party/embedly.min.js";
-import "froala-editor/js/plugins/fullscreen.min.js";
-
-// Require Editor CSS files.
-import "froala-editor/css/froala_style.min.css";
-import "froala-editor/css/froala_editor.pkgd.min.css";
-import "froala-editor/css/third_party/embedly.min.css";
-import "froala-editor/css/plugins/fullscreen.min.css";
 
 import "font-awesome/css/font-awesome.min.css";
 
-import Froala from "froala-editor";
-import FroalaEditor from "react-froala-wysiwyg";
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
 
 class TextEditor extends React.Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.handleModelChange = this.handleModelChange.bind(this);
-    this.handleUploadFile = this.handleUploadFile.bind(this);
-    this.handleDownloadFile = this.handleDownloadFile.bind(this);
-    this.state = {
-      model: "<h1>Example text</h1>"
-    };
-  }
+        this.handleModelChange = this.handleModelChange.bind(this);
+        this.handleUploadFile = this.handleUploadFile.bind(this);
+        this.handleDownloadFile = this.handleDownloadFile.bind(this);
+        this.state = {
+            model: "<h1>Example text</h1>"
+        };
+    }
 
-  handleModelChange(model) {
-    console.log(model);
-    this.setState({
-      model: model
-    });
-  }
+    handleModelChange(model) {
+        console.log(model);
+        this.setState({
+            model: model
+        });
+    }
 
-  handleUploadFile(e) {
-    const reader = new FileReader();
-    reader.onload = e => {
-      this.setState({
-        model: e.target.result
-      });
-    };
-    reader.readAsText(e.target.files[0]);
-  }
+    handleUploadFile(e) {
+        const reader = new FileReader();
+        reader.onload = e => {
+            this.setState({
+                model: e.target.result
+            });
+        };
+        reader.readAsText(e.target.files[0]);
+    }
 
-  handleDownloadFile() {
-    this.saveFile("download.html", this.state.model);
-  }
+    handleDownloadFile() {
+        this.saveFile("download.html", this.state.model);
+    }
 
-  saveFile(fileName, content) {
-    const blob = new Blob([content], { type: "text/plain" });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.download = fileName;
-    link.href = url;
-    link.click();
-  }
+    saveFile(fileName, content) {
+        const blob = new Blob([content], { type: "text/plain" });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.download = fileName;
+        link.href = url;
+        link.click();
+    }
 
-  handleOpenUploadFile() {
-    document.getElementById("file").click();
-  }
+    handleOpenUploadFile() {
+        document.getElementById("file").click();
+    }
 
-  render() {
-    Froala.DefineIcon("upload", {
-      FA5NAME: "upload",
-      template: "font_awesome_5r"
-    });
-    Froala.RegisterCommand("upload", {
-      title: "Upload",
-      icon: "upload",
-      focus: true,
-      undo: false,
-      refreshAfterCallback: true,
-      callback: () => {
-        this.handleOpenUploadFile();
-      }
-    });
-    Froala.DefineIcon("download", {
-      FA5NAME: "download",
-      template: "font_awesome_5r"
-    });
-    Froala.RegisterCommand("download", {
-      title: "Download",
-      icon: "download",
-      focus: true,
-      undo: false,
-      refreshAfterCallback: true,
-      callback: () => {
-        this.handleDownloadFile();
-      }
-    });
-    return (
-      <div className="body-container">
-        <input
-          type="file"
-          id="file"
-          style={{ display: "none" }}
-          onChange={e => this.handleUploadFile(e)}
-        />
-        <FroalaEditor
-          model={this.state.model}
-          onModelChange={this.handleModelChange}
-          tag="textarea"
-          config={{
-            attribution: false,
-            placeholder: "Start typing...",
-            toolbarButtons: {
-              moreText: {
-                buttons: [
-                  "bold",
-                  "italic",
-                  "underline",
-                  "strikeThrough",
-                  "fontFamily",
-                  "fontSize",
-                  "textColor",
-                  "backgroundColor",
-                  "subscript",
-                  "superscript",
-                  "inlineClass",
-                  "inlineStyle",
-                  "clearFormatting"
-                ],
-                buttonsVisible: 5
-              },
-              moreParagraph: {
-                buttons: [
-                  "alignLeft",
-                  "alignCenter",
-                  "alignRight",
-                  "alignJustify",
-                  "formatOL",
-                  "formatUL",
-                  "paragraphFormat",
-                  "paragraphStyle",
-                  "lineHeight",
-                  "outdent",
-                  "indent",
-                  "quote"
-                ],
-                buttonsVisible: 6
-              },
-              moreRich: {
-                buttons: [
-                  "insertLink",
-                  "insertImage",
-                  "insertVideo",
-                  "insertTable",
-                  "emoticons",
-                  "fontAwesome",
-                  "specialCharacters",
-                  "embedly",
-                  "insertFile",
-                  "insertHR"
-                ],
-                buttonsVisible: 6
-              },
-              moreMisc: {
-                buttons: [
-                  "undo",
-                  "redo",
-                  "upload",
-                  "download",
-                  "fullscreen",
-                  "print",
-                  "getPDF",
-                  "spellChecker",
-                  "selectAll",
-                  "html",
-                  "help"
-                ],
-                align: "right",
-                buttonsVisible: 4
-              }
+    render() {
+        const CustomToolbar = () => (
+            <div id="toolbar">
+                <span className="ql-formats">
+                    <select className="ql-header" defaultValue={""} onChange={e => e.persist()}>
+                        <option value="1"></option>
+                        <option value="2"></option>
+                        <option selected></option>
+                    </select>
+                </span>
+                <span className="ql-formats">
+                    <button className="ql-bold"></button>
+                    <button className="ql-italic"></button>
+                    <button className="ql-underline"></button>
+                    <button className="ql-strike"></button>
+                </span>
+                <span className="ql-formats">
+                    <button className="ql-blockquote"></button>
+                    <select className="ql-color">
+                        <option value="red"></option>
+                        <option value="green"></option>
+                        <option value="blue"></option>
+                        <option value="orange"></option>
+                        <option value="violet"></option>
+                        <option value="#d0d1d2"></option>
+                        <option selected></option>
+                    </select>
+                    <button className="ql-insertStar">
+                        <span className="fa fa-plus" />
+                    </button>
+                </span>
+            </div >
+        );
+        function insertStar() {
+            const cursorPosition = this.quill.getSelection().index
+            this.quill.insertText(cursorPosition, "★")
+            this.quill.setSelection(cursorPosition + 1)
+        }
+        const formats = [
+            "header",
+            "font",
+            "size",
+            "bold",
+            "italic",
+            "underline",
+            "strike",
+            "blockquote",
+            "list",
+            "bullet",
+            "indent",
+            "link",
+            "image",
+            "color"
+        ];
+
+        const modules = {
+            toolbar: {
+                container: "#toolbar",
+                handlers: {
+                    insertStar: insertStar
+                }
             },
-            pluginsEnabled: [
-              "table",
-              "spell",
-              "quote",
-              "save",
-              "quickInsert",
-              "paragraphFormat",
-              "paragraphStyle",
-              "help",
-              "draggable",
-              "align",
-              "link",
-              "lists",
-              "file",
-              "image",
-              "emoticons",
-              "url",
-              "video",
-              "embedly",
-              "colors",
-              "entities",
-              "inlineClass",
-              "inlineStyle",
-              // 'codeBeautif '
-              // 'spellChecker',
-              "imageTUI"
-            ]
-          }}
-        />
-      </div>
-    );
-  }
+            clipboard: {
+                matchVisual: false,
+            }
+        };
+        return (
+            <div className="body-container">
+                <input
+                    type="file"
+                    id="file"
+                    style={{ display: "none" }}
+                    onChange={e => this.handleUploadFile(e)}
+                />
+                <CustomToolbar />
+                <ReactQuill modules={modules} formats={formats} value={this.state.model}
+                    onChange={this.handleModelChange} />
+            </div>
+        );
+    }
 }
 
 export default TextEditor;
